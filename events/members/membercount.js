f2df1919244a0c20686053d0115dadc8e1dfdd88
Rdimo <rasmus.nyblom@hotmail.com>
2022-01-07 13:39:02 +0100

    fixes

--- events/members/membercount.js
+++ events/members/membercount.js
@@ -1,8 +1,8 @@
 module.exports = (client) => {
-      setInterval(() =>{
-        let myGuild = client.guilds.cache.get("899560455993966633"); //guild id
+    let myGuild = client.guilds.cache.get("899560455993966633") //your guild id
+    setInterval(() => {
         let memberCount = myGuild.members.cache.filter(member => !member.user.bot).size.toLocaleString();
         let memberCountChannel = myGuild.channels.cache.get("905224390092939424"); //channel id (recommend to have this id as a voice channel id)
-        memberCountChannel.setName(`👥・${memberCount}`);
-    }, 30000); //every 30 seconds, keep it around here so your bot don't get banned for api abuse
+        memberCountChannel.setName(`👥〢${memberCount}`); //name of the channel
+    }, 15000); //every 15 seconds, keeping it around here is good
 }
